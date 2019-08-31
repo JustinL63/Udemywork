cd8f8d2b0ad7f4a520945b117f292202e3bd6213
Justin Lamb <lambjg63@gmail.com>
2019-08-31 17:07:59 -0400

    somethingsomethingsomethingdarkside

---
+++ complete-react-guide/src/App.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from "./Person/Person"
+
+class App extends Component {
+  state = {
+    persons: [
+      {name: "Justin", age: 37, },
+      {name: "Harper", age: 2, },
+      {name: "Kelsey", age: 27, }
+    ]
+  }
+
+
+nameChangeHandler = (event) => {
+  this.setState({
+    persons: [
+      {name: "Hector", age: 25},
+      {name: "Pubert", age: 45},
+      {name: event.target.value, age: 68}
+    ],
+    showPersons: false
+  })
+}
+
+  deletePersonHandler = (personIndex) => {
+    //const persons = this.state.persons.slice()
+      const persons = [...this.state.persons]
+      persons.splice(personIndex, 1)
+      this.setState({persons: persons})
+  }
+
+  togglePersonsHandler = () => {
+        const doesShow = this.state.showPersons
+        this.setState({showPersons: !doesShow})
+  }
+
+  render() {  
+    const style = {
+      backgroundColor: "white", 
+      font: "inherit",
+      border: "1px solid blue",
+      padding: "8px",
+      cursor: "pointer"
+
+    }
+
+    let persons = null
+      if (this.state.showPersons) {
+          persons = (
+            <div >
+            {this.state.persons.map((person, index) => {
+              return <Person 
+                click={() => this.deletePersonHandler(index)}
+                name={person.name}
+                age={person.age}/>
+            })}
+          </div> 
+          )
+      }
+
+
+    return (
+      <div className="App">
+       <h1>Hi, I'm a React App!</h1>
+       <p>Another Heading</p>
+       <button
+       style={style}
+       onClick={this.togglePersonsHandler} >Toggle Persons</button>
+       {persons}    
+      
+      </div>
+    );
+    
+     // return React.createElement('div', {className: "App"}, React.createElement("h1", null, "Hi I'm a React App!!"))
+
+  }
+}
+
+export default App;
