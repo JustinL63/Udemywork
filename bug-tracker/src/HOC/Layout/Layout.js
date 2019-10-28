78de57eeb7ef79d8da7b8fc56e139a0689e6c1e7
Justin Lamb <lambjg63@gmail.com>
2019-10-28 11:56:19 -0400

    layout

---
+++ bug-tracker/src/HOC/Layout/Layout.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react"
+import Aux from "../Auxillary/Auxillary"
+
+class Layout extends Component {
+    render() {
+        return(
+            <Aux>
+                <h2>Toolbar</h2>
+                <h2>SideDrawer</h2>
+            </Aux>
+        )
+    }
+}
+
+export default Layout
